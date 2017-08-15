2887e75bc2e27c922e0bd1f5ecee4b220863f64b
Dominik Herold <d.herold@cid.biz>
2017-08-15 13:36:29 +0200

    init

---
+++ Klo.Tray/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Klo.Tray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyWhenFreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fehler";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.notifyWhenFreeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 70);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // notifyWhenFreeToolStripMenuItem
+            // 
+            this.notifyWhenFreeToolStripMenuItem.Name = "notifyWhenFreeToolStripMenuItem";
+            this.notifyWhenFreeToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.notifyWhenFreeToolStripMenuItem.Text = "Notify when free";
+            this.notifyWhenFreeToolStripMenuItem.Click += new System.EventHandler(this.notifyWhenFreeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem notifyWhenFreeToolStripMenuItem;
+
+    }
+}
+
---
+++ Klo.Tray/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Media;
+using System.Timers;
+using System.Windows.Forms;
+
+using Klo.Core;
+
+namespace Klo.Tray
+{
+    public partial class Form1 : Form
+    {
+        private readonly System.Timers.Timer _timer;
+        private readonly WebClientWrapper _client;
+        private readonly Core.Klo _klo;
+
+        private bool _notificationWanted;
+        private bool? _lastState = false;
+        private DateTime _inUseSince = DateTime.Now;
+
+        public Form1()
+        {
+            var host = ConfigurationManager.AppSettings["host"];
+            InitializeComponent();
+            _client = new WebClientWrapper();
+            _klo = new Core.Klo(_client, host);
+
+            _timer = new System.Timers.Timer(5000) { AutoReset = false };
+            _timer.Elapsed += OnTimeEvent;
+            OnTimeEvent(null, null);
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            lock (this)
+            {
+                try
+                {
+                    _lastState = _klo.IsInUse();
+                    if (!_lastState.HasValue)
+                    {
+                        notifyIcon1.Icon = new Icon("warning.ico");
+                        notifyIcon1.Text = "Fehler";
+                    }
+                    else if (_lastState.GetValueOrDefault(true))
+                    {
+                        notifyIcon1.Icon = new Icon("trafficlight_red_16.ico");
+                        notifyIcon1.Text = "Besetzt seit " + Math.Round((DateTime.Now - _inUseSince).TotalMinutes, 0) + " Minuten";
+                    }
+                    else
+                    {
+                        notifyIcon1.Icon = new Icon("trafficlight_green_16.ico");
+                        notifyIcon1.Text = "Frei";
+                        _inUseSince = DateTime.Now;
+
+                        if (_notificationWanted)
+                        {
+                            _notificationWanted = false;
+                            SendFreeNotification();
+                        }
+                    }
+                }
+                finally
+                {
+                    _timer.Enabled = true;
+                }
+            }
+        }
+
+        private void SendFreeNotification()
+        {
+            notifyIcon1.ShowBalloonTip(10000, "Erleichterung naht", "KLO ist frei! :-)", ToolTipIcon.Info);
+            new SoundPlayer("spuelung.wav").Play();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+                return;
+
+            notifyWhenFreeToolStripMenuItem.Enabled = _lastState.GetValueOrDefault(false);
+            notifyWhenFreeToolStripMenuItem.Checked = _notificationWanted;
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OnTimeEvent(null, null);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyWhenFreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!_lastState.GetValueOrDefault())
+                return;
+
+            _notificationWanted = true;
+            notifyWhenFreeToolStripMenuItem.Checked = true;
+            notifyIcon1.ShowBalloonTip(2000, "Besetzt!", "Aber du wirst benachrichtigt, sobald KLO frei ist!", ToolTipIcon.Info);
+        }
+    }
+}
